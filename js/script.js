17a2de602041ffc7332137e323064e29c2353c5c
seongho <damadu01@gmail.com>
2023-05-23 23:12:57 +0900

    aside 완성

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
+// more-btn
 let moreBtn = $('.more-btn-wrap');
+$('.aside').hide();
 
 moreBtn.on({click:function(){
     $('.bar').toggleClass('toggle');
+    $('.aside').slideDown();
+    $('.aside-lnb').addClass('active');
+}})
+
+// aside
+let close = $('.aside-close');
+close.on({click:function(){
+    $('.aside-lnb').removeClass('active');
+    $('.bar1').removeClass('toggle');
+    $('.bar2').removeClass('toggle');
+    $('.bar3').removeClass('toggle');
+    $('.aside').delay(1000).hide();
+}})
+
+// aside ul
+let aside_ul = $('.depth');
+$('.depth>ul').hide();
+aside_ul.on({click:function(){
+    $('.depth>a').not($(this).find('a').toggleClass('active'));
+    // $('.depth>ul').slideToggle();
+    $('.depth>ul').not($(this).find('ul').slideToggle());
 }})
