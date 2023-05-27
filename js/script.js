406750120c867055bbc8f25985661e9c34950788
seongho <damadu01@gmail.com>
2023-05-28 01:30:09 +0900

    배너완성(반응형 사이즈 조절하기)

--- js/script.js
+++ js/script.js
@@ -2,37 +2,45 @@
 let moreBtn = $('.more-btn-wrap');
 $('.aside').hide();
 
-moreBtn.on({click:function(){
-    $('.bar').toggleClass('toggle');
-    $('.aside').slideDown();
-    $('.aside-lnb').addClass('active');
-}})
+moreBtn.on({
+    click: function () {
+        $('.bar').toggleClass('toggle');
+        $('.aside').slideDown();
+        $('.aside-lnb').addClass('active');
+    }
+})
 
 // aside
 let close = $('.aside-close');
-close.on({click:function(){
-    $('.aside-lnb').removeClass('active');
-    $('.bar1').removeClass('toggle');
-    $('.bar2').removeClass('toggle');
-    $('.bar3').removeClass('toggle');
-    $('.aside-bg').delay(1000).hide(); 
-}})
+close.on({
+    click: function () {
+        $('.aside-lnb').removeClass('active');
+        $('.bar1').removeClass('toggle');
+        $('.bar2').removeClass('toggle');
+        $('.bar3').removeClass('toggle');
+        $('.aside-bg').delay(1000).hide();
+    }
+})
 
 // aside ul
 let aside_ul = $('.depth');
 $('.depth>ul').hide();
-aside_ul.on({click:function(){
-    $('.depth>a').not($(this).find('a').toggleClass('active'));
-    // $('.depth>ul').slideToggle();
-    $('.depth>ul').not($(this).find('ul').slideToggle());
-}})
+aside_ul.on({
+    click: function () {
+        $('.depth>a').not($(this).find('a').toggleClass('active'));
+        // $('.depth>ul').slideToggle();
+        $('.depth>ul').not($(this).find('ul').slideToggle());
+    }
+})
 
 // banner
 $('.main-vi-wrap').slick({
     dots: true,
     infinite: true,
     speed: 500,
-    fade: true,
-    cssEase: 'linear',
-    arrows : false
-  });
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+});
