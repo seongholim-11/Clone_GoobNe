c132d8aacfb2feed84afa42e1c943a7e0bbeb0ee
seongho <damadu01@gmail.com>
2023-05-29 11:32:15 +0900

    드롭헤더 완성

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+// header(drop)
+let header = document.querySelector('#header');
+let headerHeight = header.offsetHeight;
+
+window.onscroll = function(){
+    let windowTop = window.scrollY;
+
+    if(windowTop >= headerHeight){
+        header.classList.add("drop");
+    }else{
+        header.classList.remove("drop")
+    }
+}
+
 // more-btn
 let moreBtn = $('.more-btn-wrap');
 $('.aside').hide();
